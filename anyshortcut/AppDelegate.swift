82e7a75979f15f56b0dd02db7124a85f708370ae
Folyd <lyshuhow@gmail.com>
2019-12-16 23:59:46 +0800

    Initial Commit

---
+++ anyshortcut/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  anyshortcut
+//
+//  Created by wichna on 12/16/19.
+//  Copyright © 2019 anyshortcut. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ anyshortcut/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  anyshortcut
+//
+//  Created by wichna on 12/16/19.
+//  Copyright © 2019 anyshortcut. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
